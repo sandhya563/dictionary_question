2a27f657b7a66f339afe542e85bd6a47df1566cd
sandhya563 <60354285+sandhya563@users.noreply.github.com>
2020-12-06 11:19:44 +0530

    Add files via upload

---
+++ update_dict.py
@@ -0,0 +1,17 @@
+data1={"first": 1} 
+data2={"second": 2} 
+data3={"third": 1}
+data4={"four": 5} 
+data5={"five": 5} 
+data6={"six": 9}
+data7={"seven": 7}
+a={}
+for i in (data1,data2,data3,data4,data5,data6,data7):
+    a.update(i)
+print(a)
+
+
+
+
+
+ 
